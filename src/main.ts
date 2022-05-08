4daac9f0fb9358c647a09d0a2508eab142a5595a
Raphael Passos <raphael251@hotmail.com>
2022-05-08 00:03:19 -0300

    chore: add swagger to the project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Producers API')
+    .setDescription('Create and manage your producers easily')
+    .setVersion('1.0')
+    .addTag('producers')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
