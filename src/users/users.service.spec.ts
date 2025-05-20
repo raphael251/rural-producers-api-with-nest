9fada3a2169973a2c650a8196851b01e785a2d7e
Raphael Passos <raphael251@hotmail.com>
2025-05-19 22:57:42 -0300

    test: fix users modules unit tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { User } from './entites/user.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+type MockType<T> = {
+  [P in keyof T]?: jest.Mock<unknown>;
+};
+
+const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
+  save: jest.fn((entity) => entity),
+}));
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
